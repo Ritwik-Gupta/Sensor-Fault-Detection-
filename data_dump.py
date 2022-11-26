b94fa7e61f861b117a8422480dd188c779d0995e
ritwik <ritwik94348gupta@gmail.com>
2022-11-26 17:15:58 +0530

    First Commit

---
+++ data_dump.py
@@ -0,0 +1,29 @@
+import pymongo
+import pandas as pd
+import json
+
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+# Database Name
+dataBase = client["SensorDB"]
+
+# Collection  Name
+collection = dataBase['SensorData']
+
+#Data file path
+file_path = "aps_failure_training_set1.csv"
+
+
+if __name__ == "__main__":
+    #data = pd.read_csv(file_path)
+    #print(data.shape)
+    
+    #data.reset_index(inplace=True, drop=True)
+
+    #json_records = list(json.loads(data.T.to_json()).values())
+
+    #print(json_records[0])
+
+    #insert data into mongodb
+    #rec = collection.insert_many(json_records)
+    pass
